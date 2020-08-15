c42e34ab68871c8c570e1f3d553a132e18c2ab16
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-15 12:00:51 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,9 +7,14 @@ class Person
 
   def get_married(person)
     self.partner = person
+    
     person.partner = self
   end
 
+end
+
+class PartnerError < StandardError
+  end
 end
 
 beyonce = Person.new("Beyonce")
